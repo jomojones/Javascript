68a01799dc81fa2d34e0d9c4986b1eeb9ea44248
jomo <mboywinthmbigent@gmail.com>
2023-12-15 12:28:02 +0300

    I have added array methods.

---
+++ app.js
@@ -0,0 +1,98 @@
+/*let firstname = "Ferdinand"
+let secondname = "Jomo"
+let lastname = "Muhia"
+let age = 22
+
+console.log(firstname)
+console.log(secondname)
+console.log(lastname)
+console.log(age)
+
+let num1 = 40
+let num2 = 50
+
+let num3=num1+num2
+let num4=num2-num1
+let num5=num1*num2
+let num6=num2/num1
+let num7=num1%num2
+
+console.log(num3,num4,num5,num6,num7)
+
+let first_name = "jomo"
+let second_name ="Jones"
+let last_name = "ferdi"
+
+console.log(first_name,second_name,last_name)
+
+function ggh(a,b,c){ 
+   firstname=a
+   secondname=b
+   lastname=c
+
+console.log(firstname,secondname,lastname)
+}
+ggh("jomo","jones","muhia")
+
+function ffg (first_name,second_name,last_name){
+jjk=first_name+second_name+last_name
+console.log(`i am called ${jjk}`)
+
+}
+ffg("jomo","jones","muhia")
+
+function area(radius){
+const pi=3.142 
+
+formula=0.5*pi*radius*radius
+console.log(formula)
+} 
+area(18)*/
+
+let person ={
+age:22,
+height: 170,
+weight: 67,
+gender:"male",
+dob:2000,
+kol:function(){
+    console.log("greetings!")
+
+}
+
+}
+
+console.log(person.kol())
+console.log(person.gender)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
